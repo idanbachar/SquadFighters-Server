3af55850f34d948866e2f6cbd17bc79dbf9c8dc5
Idan <idan_bachar@hotmail.com>
2019-02-23 19:48:07 +0200

    Start Server

---
+++ Server/BattleRoyaleProjectServer/BattleRoyaleProjectServer/Server.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BattleRoyaleProjectServer
+{
+    public class Server
+    {
+        private TcpListener listener;
+        private string serverIp;
+        private int serverPort;
+        private Dictionary<string, TcpClient> clients;
+
+        public Server(string ip, int port)
+        {
+
+            this.serverIp = ip;
+            this.serverPort = port;
+            this.clients = new Dictionary<string, TcpClient>();
+        }
+
+        public void Start()
+        {
+
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse(serverIp), serverPort);
+                listener.Start();
+
+                Console.WriteLine("Server started on ip '" + serverIp + "' and port '" + serverPort + ".");
+
+                new Thread(WaitForConnections).Start();
+                new Thread(Chat).Start();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        public void WaitForConnections()
+        {
+
+            while (true)
+            {
+
+                Console.WriteLine("Waiting for connections..");
+                TcpClient client = listener.AcceptTcpClient();
+                string clientIp = ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString();
+
+                if (!clients.ContainsKey(clientIp))
+                {
+
+                    clients.Add(clientIp, client);
+                    new Thread(() => Recieve(clients[clientIp])).Start();
+                    Console.WriteLine(clientIp + " connected to the server.");
+                }
+                else
+                {
+                    Console.WriteLine("You are already connected to the server!");
+                }
+
+            }
+
+        }
+
+        public void Recieve(TcpClient client)
+        {
+
+            while (true)
+            {
+
+                try
+                {
+
+                    NetworkStream netStream = client.GetStream();
+                    byte[] bytes = new byte[1024];
+                    netStream.Read(bytes, 0, bytes.Length);
+                    string data = Encoding.ASCII.GetString(bytes);
+                    string message = data.Substring(0, data.IndexOf("\0"));
+                    Console.WriteLine("<Client>: " + message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        public void Chat()
+        {
+
+            while (true)
+            {
+
+                string message = Console.ReadLine();
+                SendAll(message);
+                Console.WriteLine("<Server>: " + message);
+            }
+        }
+
+        public void SendAll(string message)
+        {
+
+            try
+            {
+                foreach (KeyValuePair<string, TcpClient> client in clients)
+                {
+
+                    NetworkStream netStream = client.Value.GetStream();
+                    byte[] bytes = Encoding.ASCII.GetBytes(message);
+                    netStream.Write(bytes, 0, bytes.Length);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        public void SendTo(TcpClient client, string message)
+        {
+
+            NetworkStream netStream = client.GetStream();
+            byte[] bytes = Encoding.ASCII.GetBytes(message);
+            netStream.Write(bytes, 0, bytes.Length);
+
+        }
+
+        public byte[] ObjectToByteArray(Object obj)
+        {
+
+            if (obj == null)
+                return null;
+
+            MemoryStream stream = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, obj);
+            byte[] arr = stream.ToArray();
+            stream.Close();
+            return arr;
+        }
+
+        public object ByteArrayToObject(byte[] Buffer)
+        {
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            MemoryStream stream = new MemoryStream(Buffer);
+            object obj = null;
+            try
+            {
+                obj = formatter.Deserialize(stream);
+            }
+            catch
+            {
+                obj = null;
+            }
+            stream.Close();
+            return obj;
+        }
+    }
+}
